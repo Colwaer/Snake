8b054a116de0d6b41de0a4850ecd7bd545848e00
Colwaer <674329253@qq.com>
2020-10-11 18:20:34 +0800

    实现开始游戏UI界面

--- Snake.cpp
+++ Snake.cpp
@@ -4,24 +4,28 @@
 #include<conio.h>
 
 const int sizeFont = 18;
-
 const int WIDTH = 1280;			// 屏幕宽1024
 const int HEIGHT = 720;			// 屏幕高576
 const int MAPW = (WIDTH * 4);	// 地图宽
 const int MAPH = (HEIGHT * 4);	// 地图高
 
+int Round = 1;//关卡
+
 IMAGE		Map(MAPW, MAPH);
 
 void DrawMap(int x1, int y1, int x2, int y2);
 void Init();
-
+void MainMenu();
+void ModeSelect();
+void RoundSelect();
 int main()
 {
     initgraph(WIDTH, HEIGHT);
     Init();
     _getch();
+    int pd = 0;
+    MainMenu();
 
-    _getch();
     closegraph();
     return 0;
 }
@@ -39,10 +43,120 @@ void Init()
     loadimage(&StartBK, _T("开始界面.png"));
     putimage(0, 0, WIDTH, HEIGHT, &StartBK, 0, 0);
     settextstyle(sizeFont*2, sizeFont, L"楷体");
-    settextcolor(0xFFB6C1);
-    outtextxy(WIDTH / 2-100, HEIGHT / 2+250, L"按任意键继续");
+    COLORREF TextColor;
+    TextColor = { 0XFFEFD1 };
+    settextcolor(TextColor);
+    outtextxy(WIDTH / 2-WIDTH/12, HEIGHT / 2+HEIGHT/3, L"按任意键继续");
 }
 void MainMenu()
+
 {
+    
+    IMAGE MainMenuBK(WIDTH, HEIGHT);
+    loadimage(&MainMenuBK, _T("MainMenuBK.png"));
+    putimage(0, 0, WIDTH, HEIGHT, &MainMenuBK, 0, 0);
+    IMAGE StartGame1, HelpGame1, QuitGame1,SettingGame1, StartGame2, HelpGame2, QuitGame2, SettingGame2;
+    loadimage(&StartGame1, _T("开始游戏1.png"));
+    loadimage(&HelpGame1, _T("帮助游戏1.png"));
+    loadimage(&QuitGame1, _T("退出游戏1.png"));
+    loadimage(&SettingGame1, _T("游戏设置1.png"));
+    loadimage(&StartGame2, _T("开始游戏2.png"));
+    loadimage(&HelpGame2, _T("帮助游戏2.png"));
+    loadimage(&QuitGame2, _T("退出游戏2.png"));
+    loadimage(&SettingGame2, _T("游戏设置2.png"));
+    putimage(WIDTH/8+WIDTH/4,HEIGHT / 4, 274, 42, &StartGame2, 0, 0);
+    putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4+HEIGHT/8, 274, 42, &SettingGame1, 0, 0);
+    putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4+HEIGHT/4, 274, 42, &HelpGame1, 0, 0);
+    putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4+HEIGHT/4+HEIGHT/8, 274, 42, &QuitGame1, 0, 0);
+//开始界面及准备
+    int pd = 1;
+    while(1)
+    {
+        int i = _getch();
+        if (i == 119||i==87) { if (pd != 1)pd--; }
+        else if (i == 115||i==83) { if (pd != 4)pd++; }
+        else if (i == 13) { break; }
+        else{}
+        if (pd == 1) {
+            putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4, 274, 42, &StartGame2, 0, 0);
+            putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 8, 274, 42, &SettingGame1, 0, 0);
+        }
+        else if (pd == 2) { 
+            putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 8, 274, 42, &SettingGame2, 0, 0);
+            putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4, 274, 42, &StartGame1, 0, 0);
+            putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 4, 274, 42, &HelpGame1, 0, 0);
+        }
+        else if (pd == 3) { 
+            putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 4, 274, 42, &HelpGame2, 0, 0); 
+            putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 8, 274, 42, &SettingGame1, 0, 0);
+            putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 4 + HEIGHT / 8, 274, 42, &QuitGame1, 0, 0);
+        }
+        else if (pd == 4) {
+            putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 4 + HEIGHT / 8, 274, 42, &QuitGame2, 0, 0);
+            putimage(WIDTH / 8 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 4, 274, 42, &HelpGame1, 0, 0);
+        }
+    }
+//第一层UI界面
+    if (pd == 1) { ModeSelect(); return; }
+//第二层UI界面
+}
+void ModeSelect()
+{
+    IMAGE gameChose(WIDTH, HEIGHT);
+    loadimage(&gameChose, _T("gameChose.png"));
+    putimage(0, 0, WIDTH, HEIGHT, &gameChose, 0, 0);
+    IMAGE RoundMode;
+    loadimage(&RoundMode, _T("chose2_1.png"));
+    putimage(WIDTH/8 + WIDTH / 4, HEIGHT / 3, 297, 74, &RoundMode, 0, 0);
+    while(1)
+    {
+        if (_getch() == 13) 
+        {
+            RoundSelect();
+            break;
+        }
+    }
+}
+void RoundSelect()
+{
+    IMAGE RoundSelect;
+    loadimage(&RoundSelect, _T("MainMenuBK.png"));
+    putimage(0, 0, WIDTH, HEIGHT, &RoundSelect, 0, 0);
+    IMAGE Level1_1, Level1_2, Level2_1, Level2_2, Level3_1, Level3_2;
+    loadimage(&Level1_1, _T("Level1_3.png"));
+    loadimage(&Level1_2, _T("Level1_2.png"));
+    loadimage(&Level2_1, _T("Level2_3.png"));
+    loadimage(&Level2_2, _T("Level2_2.png"));
+    loadimage(&Level3_1, _T("Level3_3.png"));
+    loadimage(&Level3_2, _T("Level3_2.png"));
+    putimage(WIDTH / 5 + WIDTH / 4, HEIGHT / 4, 102, 103, &Level1_2, 0, 0);
+    putimage(WIDTH / 5 + WIDTH / 4, HEIGHT / 4+ HEIGHT / 4, 102, 103, &Level2_1, 0, 0);
+    putimage(WIDTH / 5 + WIDTH / 4, HEIGHT / 4+ HEIGHT / 2, 102, 103, &Level3_1, 0, 0);
+    int pd = 1;
+    while(1)
+    {
+        int i = _getch();
+        if (i == 119 || i == 87) { if (pd != 1)pd--; }
+        else if (i == 115 || i == 83) { if (pd != 3)pd++; }
+        else if (i == 13) { break; }
+        else {}
+        if (pd == 1) {
+            putimage(WIDTH / 5 + WIDTH / 4, HEIGHT / 4, 102, 103, &Level1_2, 0, 0);
+            putimage(WIDTH / 5 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 4, 102, 103, &Level2_1, 0, 0);
+            Round = 1;
+        }
+        else if (pd == 2) {
+            putimage(WIDTH / 5 + WIDTH / 4, HEIGHT / 4, 102, 103, &Level1_1, 0, 0);
+            putimage(WIDTH / 5 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 4, 102, 103, &Level2_2, 0, 0);
+            putimage(WIDTH / 5 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 2, 102, 103, &Level3_1, 0, 0);
+            Round = 2;
+        }
+        else if (pd == 3) {
+            putimage(WIDTH / 5 + WIDTH / 4, HEIGHT / 4, 102, 103, &Level1_1, 0, 0);
+            putimage(WIDTH / 5 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 4, 102, 103, &Level2_1, 0, 0);
+            putimage(WIDTH / 5 + WIDTH / 4, HEIGHT / 4 + HEIGHT / 2, 102, 103, &Level3_2, 0, 0);
+            Round = 3;
+        }
+    }
 
 }
